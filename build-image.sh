0f8e51222904da98e34ac609af26c5b0479ddbd7
nightwhite <nightwhite634@gmail.com>
2025-01-21 06:56:22 +0800

    同步actions 脚本

--- build-image.sh
+++ build-image.sh
@@ -5,7 +5,7 @@
 version=$(node -p "require('./package.json').version")
 
 # build main image
-docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/lafyun/runtime-node:$version -f Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/nightwhite634/laf-runtime:$version -f Dockerfile .
 
 # build init image
-docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/lafyun/runtime-node-init:$version -f Dockerfile.init .
+docker buildx build --platform linux/amd64,linux/arm64 --push -t docker.io/nightwhite634/laf-runtime-init:$version -f Dockerfile.init .
